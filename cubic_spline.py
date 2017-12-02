f8f64f1edd77b8da6fba1a210dce84483b151251
Kretov Nikita <kretov995@gmail.com>
2017-12-02 17:08:30 +0300

    Update cubic_spline.py

--- cubic_spline.py
+++ cubic_spline.py
@@ -1,107 +1,77 @@
-import matplotlib.pyplot as plt
-from pylab import arange
-from math import e
-from math import pi
-from math import sin
-from math import cos
-from numpy import poly1d
+def cubic_spline(x, y):
+  """
+  Parameters
+  ----------
+  x  : list of floats
+  y  : list of floats
 
-# need some zero vectors...
-def zeroV(m):
-    z = [0]*m
-    return(z)
-
-#INPUT: n; x0, x1, ... ,xn; a0 = f(x0), a1 =f(x1), ... , an = f(xn).
-def cubic_spline(n, xn, a):
-
-    h = zeroV(n-1)
-
-    # alpha will be values in a system of eq's that will allow us to solve for c
-    # and then from there we can find b, d through substitution.
-    alpha = zeroV(n-1)
-
-    # l, u, z are used in the method for solving the linear system
-    l = zeroV(n+1)
-    u = zeroV(n)
-    z = zeroV(n+1)
-
-    # b, c, d will be the coefficients along with a.
-    b = zeroV(n)     
-    c = zeroV(n+1)
-    d = zeroV(n)    
-
-    for i in range(n-1):
-        # h[i] is used to satisfy the condition that 
-        # Si+1(xi+l) = Si(xi+l) for each i = 0,..,n-1
-        # i.e., the values at the knots are "doubled up"
-        h[i] = xn[i+1]-xn[i]  
-
-    for i in range(1, n-1):
-        # Sets up the linear system and allows us to find c.  Once we have 
-        # c then b and d follow in terms of it.
-        alpha[i] = (3./h[i])*(a[i+1]-a[i])-(3./h[i-1])*(a[i] - a[i-1])
-
-    # I, II, (part of) III Sets up and solves tridiagonal linear system...
-    # I   
-    l[0] = 1      
-    u[0] = 0      
-    z[0] = 0
-
-    # II
-    for i in range(1, n-1):
-        l[i] = 2*(xn[i+1] - xn[i-1]) - h[i-1]*u[i-1]
-        u[i] = h[i]/l[i]
-        z[i] = (alpha[i] - h[i-1]*z[i-1])/l[i]
-
-    l[n] = 1
-    z[n] = 0
-    c[n] = 0
-
-    # III... also find b, d in terms of c.
-    for j in range(n-2, -1, -1):      
-        c[j] = z[j] - u[j]*c[j+1]
-        b[j] = (a[j+1] - a[j])/h[j] - h[j]*(c[j+1] + 2*c[j])/3.
-        d[j] = (c[j+1] - c[j])/(3*h[j]) 
-
-    # Now that we have the coefficients it's just a matter of constructing
-    # the appropriate polynomials and graphing.
-    for j in range(n-1):
-        cub_graph(a[j],b[j],c[j],d[j],xn[j],xn[j+1])
-
-    plt.show()
-
-def cub_graph(a,b,c,d, x_i, x_i_1):
-    """cub_graph takes the i'th coefficient set along with the x[i] and x[i+1]'th
-       data pts, and constructs the polynomial spline between the two data pts using
-       the poly1d python object (which simply returns a polynomial with a given root."""
-
-    # notice here that we are just building the cubic polynomial piece by piece
-    root = poly1d(x_i,True)
-    poly = 0
-    poly = d*(root)**3
-    poly = poly + c*(root)**2
-    poly = poly + b*root
-    poly = poly + a
-
-    # Set up our domain between data points, and plot the function
-    pts = arange(x_i,x_i_1, 0.001)
-    plt.plot(pts, poly(pts), '-')
-    return
+  Returns
+  -------  
+  list of list of floats
+  """
+  n = len(x) - 1
+  h = [x[i+1]-x[i] for i in range(n)]
+  al = [3*((y[i+1]-y[i])/h[i] - (y[i]-y[i-1])/h[i-1]) for i in range(1,n)]
+  al.insert(0,0)
   
-  # These are our data points
-x_vals = [0, 1./6, 1./3, 1./2, 7./12, 2./3, 3./4, 5./6, 11./12, 1]
+  l = [1] * (n+1)
+  u = [0] * (n+1)
+  z = [0] * (n+1)
+  for i in range(1, n):
+    l[i] = 2*(x[i+1]-x[i-1]) - h[i-1]*u[i-1]
+    u[i] = h[i]/l[i]
+    z[i] = (al[i] - h[i-1]*z[i-1])/l[i]
 
-# Set up the domain
-x_domain = arange(0,2, 1e-2)
+  b = [0] * (n+1)
+  c = [0] * (n+1)
+  d = [0] * (n+1)
+  for i in range(n-1, -1, -1):    #for i in reversed(range(n)):
+    c[i] = z[i] - u[i]*c[i+1]
+    b[i] = (y[i+1]-y[i])/h[i] - h[i]*(c[i+1] + 2*c[i])/3
+    d[i] = (c[i+1]-c[i])/(3*h[i])
+  return [y, b, c, d]
+  
+if __name__ == '__main__':
+  import math  
+  import matplotlib.pyplot as plt
+  import numpy as np
 
-fx = zeroV(10)
+  # the function to be interpolated
+  def f(x):
+    return math.e ** x
+    
+  # input
+  interval = 3
+  x = [i for i in range(interval + 1)]
+  y = [f(i) for i in range(interval + 1)]
 
-# Defines the function so we can get our fx values
-def sine_func(x):
-    return(1.6*e**(-2*x)*sin(3*pi*x))
+  # process
+  a = cubic_spline(x, y)
 
-for i in range(len(x_vals)):
-    fx[i] = sine_func(x_vals[i])
+  # prepare data for plotting the splines
+  points_per_interval = 5
+  xs = []
+  ys = []
+  for i in range(3):
+    xs.append(np.linspace(i, i+1, points_per_interval))
+    ys.append([a[0][i] + 
+               a[1][i]*(xs[i][k]-i) + 
+               a[2][i]*(xs[i][k]-i)**2 + 
+               a[3][i]*(xs[i][k]-i)**3   
+               for k in range(points_per_interval)])
+  
+  # prepare data for plotting the given function
+  x = np.linspace(0, 3, interval * points_per_interval - (interval - 1))
+  y = [f(x[i]) for i in range(len(x))]
 
-# Run cubic_spline interpolant.
-cubic_spline(10,x_vals,fx)
+  plt.plot(x, y, 'k.-', xs[0], ys[0], 'r.--', xs[1], ys[1], 'g.--', xs[2], ys[2], 'b.--')
+
+  plt.title('Cubic Spline')
+  plt.xlabel('x')
+  plt.ylabel('e^x')
+  plt.show()
+  
+# cubic spline interpolation
+# author : worasait suwannik
+# date   : apr 2015
+
