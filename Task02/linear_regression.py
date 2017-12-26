a4b474e92dfaa810bc913026e4f5318502ae8e67
geomodeling <34854580+geomodeling@users.noreply.github.com>
2017-12-26 14:41:04 +0300

    Update linear_regression.py

--- Task02/linear_regression.py
+++ Task02/linear_regression.py
@@ -1,80 +1,53 @@
-# Standalone simple linear regression example
-from math import sqrt
-import matplotlib.pyplot as plt
 import numpy as np
-
-# Calculate the mean value of a list of numbers
-def mean(values):
-	return sum(values) / float(len(values))
+import matplotlib.pyplot as plt
  
-# Calculate covariance between x and y
-def covariance(x, mean_x, y, mean_y):
-	covar = 0.0
-	for i in range(len(x)):
-		covar += (x[i] - mean_x) * (y[i] - mean_y)
-	return covar
+def estimate_coef(x, y):
+    # number of observations/points
+    n = np.size(x)
  
-# Calculate the variance of a list of numbers
-def variance(values, mean):
-	return sum([(x-mean)**2 for x in values])
+    # mean of x and y vector
+    m_x, m_y = np.mean(x), np.mean(y)
  
-# Calculate coefficients
-def coefficients(dataset):
-	x = [row[0] for row in dataset]
-	y = [row[1] for row in dataset]
-	x_mean, y_mean = mean(x), mean(y)
-	b1 = covariance(x, x_mean, y, y_mean) / variance(x, x_mean)
-	b0 = y_mean - b1 * x_mean
-	return [b0, b1]
-
-# Calculate root mean squared error
-def rmse_metric(actual, predicted):
-	sum_error = 0.0
-	for i in range(len(actual)):
-		prediction_error = predicted[i] - actual[i]
-		sum_error += (prediction_error ** 2)
-	mean_error = sum_error / float(len(actual))
-	return sqrt(mean_error)
+    # calculating cross-deviation and deviation about x
+    SS_xy = np.sum(y*x - n*m_y*m_x)
+    SS_xx = np.sum(x*x - n*m_x*m_x)
  
-##########################################
-# Plot linear regression
-##########################################
-def simple_linear_regression(train, test):
-	predictions = list()
-	x_arr = list()
-	b0, b1 = coefficients(train)
-	for row in test:
-		x = row[0]
-		yhat = b0 + b1 * x
-		predictions.append(yhat)
-		x_arr.append(x)
-	plt.plot(test, 'ro')
-	plt.plot([x - 1 for x in x_arr], [pr  for pr in predictions])
-	plt.show()
-	return predictions
+    # calculating regression coefficients
+    b_1 = SS_xy / SS_xx
+    b_0 = m_y - b_1*m_x
  
-##########################################
-# Compute linear regression
-##########################################
-def evaluate_algorithm(dataset, algorithm):
-	test_set = list()
-	for row in dataset:
-		row_copy = list(row)
-		row_copy[-1] = None
-		test_set.append(row_copy)
-	predicted = algorithm(dataset, test_set)
-	print(predicted)
-	actual = [row[-1] for row in dataset]
-	rmse = rmse_metric(actual, predicted)
-	return rmse, predicted
-
-##########################################
-# Test data
-##########################################
-dataset = np.array([[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]])
-
-##########################################
-# Output
-##########################################
-rmse, predicted = evaluate_algorithm(dataset, simple_linear_regression)
-print('RMSE: %.3f' % (rmse))
+    return(b_0, b_1)
+ 
+def plot_regression_line(x, y, b):
+    # plotting the actual points as scatter plot
+    plt.scatter(x, y, color = "m",
+               marker = "o", s = 30)
+ 
+    # predicted response vector
+    y_pred = b[0] + b[1]*x
+ 
+    # plotting the regression line
+    plt.plot(x, y_pred, color = "g")
+ 
+    # putting labels
+    plt.xlabel('x')
+    plt.ylabel('y')
+ 
+    # function to show plot
+    plt.show()
+ 
+def main():
+    # observations
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+ 
+    # estimating coefficients
+    b = estimate_coef(x, y)
+    print("Estimated coefficients:\nb_0 = {}  \
+          \nb_1 = {}".format(b[0], b[1]))
+ 
+    # plotting regression line
+    plot_regression_line(x, y, b)
+ 
+if __name__ == "__main__":
+    main()
